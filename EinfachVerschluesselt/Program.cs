02b083b5094311043f6c3ebbd6c4086d32f29b5f
Jan Suchotzki <jan@lernmoment.de>
2017-12-18 12:34:06 +0100

    Macht Standard EIn- & Ausgabe

--- EinfachVerschluesselt/Program.cs
+++ EinfachVerschluesselt/Program.cs
@@ -7,6 +7,12 @@ namespace EinfachVerschluesselt
         static void Main(string[] args)
         {
             Console.WriteLine("Text (unsicher) verschlüsseln mit ROT-13!");
+            Console.WriteLine();
+
+            // Deine Lösung hier !!!
+
+            Console.WriteLine("Zum Beenden bitte 'Enter' drücken");
+            Console.ReadLine();
         }
     }
 }
